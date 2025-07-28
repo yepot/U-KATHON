70f94a2ed45f5f0543efb35ef0cc47aed0382bfc
yepot <yes03326@gmail.com>
2025-07-29 00:11:03 +0900

    [Chore] 초기 환경 세팅

---
+++ kongtteok/src/main/java/com/ukathon/kongtteok/KongtteokApplication.java
@@ -0,0 +1,13 @@
+package com.ukathon.kongtteok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KongtteokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KongtteokApplication.class, args);
+	}
+
+}
---
+++ kongtteok/src/test/java/com/ukathon/kongtteok/KongtteokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukathon.kongtteok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongtteokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
